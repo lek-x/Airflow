4ac8625ac1803cb790b6967fcd57282b1568ad11
Roman <mmm.romka@gmail.com>
2023-08-27 12:15:40 +0300

    added backend S3

---
+++ backend.tf
@@ -0,0 +1,13 @@
+terraform {
+ backend "s3" {
+    endpoint                    = "https://terr-state.fra1.digitaloceanspaces.com"
+    bucket                      = "tf-store"
+    key                         = "terraform-test.tfstate"
+    region                      = "fra1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+
+  }
+}
