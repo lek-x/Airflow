c73ed7f49f7cf46ca3e06a9ea26e09b38fef40fd
Roman Mezentsev <34778894+lek-x@users.noreply.github.com>
2023-09-01 23:24:14 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,15 +1,12 @@
 terraform {
   backend "s3" {
-    endpoint                    = "https://tfstat-buck.fra1.digitaloceanspaces.com"
+    endpoint                    = "https://fra1.digitaloceanspaces.com"
     bucket                      = "tfstat-buck"
-    key                         = "workspaces/terraform.tfstate"
+    key                         = "terraform.tfstate"
     region                      = "fra1"
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-    workspaces {
-      prefix = "airflow-"
-    }
+    skip_requesting_account_id  = true
+    skip_get_ec2_platforms      = true
   }
 }
