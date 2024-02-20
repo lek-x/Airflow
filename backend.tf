20544a76e75f9cdae7e527af86c133cc5ded4ec1
Roman Mezentsev <34778894+lek-x@users.noreply.github.com>
2024-02-20 22:34:49 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     endpoint                    = "https://fra1.digitaloceanspaces.com"
-    bucket                      = "tfstat-buck"
+    bucket                      = "tfstate-air"
     key                         = "terraform.tfstate"
     region                      = "fra1"
     skip_region_validation      = true
